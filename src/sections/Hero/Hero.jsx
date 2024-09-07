3b05ca46e8a55bf11a9e984a0d5ea52912c7b6dd
Ankit Kumar Biswal <ankitkumarbiswal04@gmail.com>
2024-09-07 12:01:55 +0530

    .

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,59 +1,67 @@
-import styles from './HeroStyles.module.css'
-import heroImg from '../.././assets/hero-img.png'
-import sun from '../../assets/sun.svg'
-import moon from '../../assets/moon.svg'
-import twitterLight from '../../assets/twitter-light.svg'
-import twitterDark from '../../assets/twitter-dark.svg'
-import githubLight from '../../assets/github-light.svg'
-import githubDark from '../../assets/github-dark.svg'
-import linkedinLight from '../../assets/linkedin-light.svg'
-import linkedinDark from '../../assets/linkedin-dark.svg'
-import CV from '../../assets/ANKIT_KUMAR_BISWAL_RESUME.pdf'
-import { useTheme } from '../../common/ThemeContext'
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/Design uten navn.png';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import twitterLight from '../../assets/twitter-light.svg';
+import twitterDark from '../../assets/twitter-dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import CV from '../../assets/ANKIT_KUMAR_BISWAL_RESUME.pdf';
+import { useTheme } from '../../common/ThemeContext';
+
 function Hero() {
-    const{theme,toggleTheme}=useTheme();
-    const themeIcon=theme==='light'?sun:moon;
-    const twitterIcon=theme==='light'?twitterLight:twitterDark;
-    const githubIcon=theme==='light'?githubLight:githubDark;
-    const linkedinIcon=theme==='light'?linkedinLight:linkedinDark;
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === 'light' ? sun : moon;
+  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+
   return (
-<section id="hero" className={styles.container}>
-    <div className={styles.colorModeContainer}>
-        <img className={styles.Hero} 
-        src={heroImg}
-        alt="This is the profile picture of Ankit Kumar Biswal" 
-         />
-    </div>
-    <div>
-        <img className={styles.colorMode}
-        src={themeIcon}
-        alt="Color mode icon"
-        onClick={toggleTheme}
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          src={heroImg}
+          className={styles.hero}
+          alt="Profile picture of Ankit Kumar Biswal"
         />
-    </div>
-    <div className={styles.info}>
-        <h1>Ankit Kumar<br/>Biswal</h1>
-        <h2>Forntend developer</h2>
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+          onClick={toggleTheme}
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Ankit Kumar
+          <br />
+          Biswal
+        </h1>
+        <h2>Frontend Developer</h2>
         <span>
-            <a href="https://twitter.com/" target="_blank" >
-                <img src={twitterIcon} alt="Twitter icon" />
-            </a>
-            <a href="https://github.com/" target="_blank">
-                <img src={githubIcon} alt="Github icon" />
-            </a>
-            <a href="https://linkedin.com/" target="_blank" >
-                <img src={linkedinIcon} alt="Linkedin icon" />
-            </a>
+          <a href="https://twitter.com/" target="_blank">
+            <img src={twitterIcon} alt="Twitter icon" />
+          </a>
+          <a href="https://github.com/" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+          <a href="https://linkedin.com/" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
         </span>
-        <p className={styles.description}>With a passion of learning and building react application</p>
-        <a href={CV} >
-        <button className="hover" download>
-            Resume
-        </button>
+        <p className={styles.description}>
+          With a passion for developing modern React web apps for commercial
+          businesses.
+        </p>
+        <a href={CV} download>
+          <button className="hover">Resume</button>
         </a>
-    </div>
-</section>  
-) ;
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
